3f5017464dae5c3148f6adcb92d35b8845467a60
Dwan <me@dwaan.com>
2022-04-18 12:04:09 +0300

    A better way to count success result by storing in array

--- index.js
+++ index.js
@@ -4,30 +4,29 @@ const EventEmitter = require(`events`);
 const { execFile } = require(`child_process`);
 
 // alive: time to take wait untill a device can be consider awake.
-//        -> Nintendo Switch have 3 - 8 seconds ping alive when it sleep
 // Emit event: 'connected', 'awake', 'sleep', `update`
 class ping extends EventEmitter {
-    constructor(ip, alive = 20, every = 4) {
-        if (!ip) return;
+	constructor(ip, alive = 20, every = 4) {
+		if (!ip) return;
 
 		super();
 
-		this.ip = ip;
-		this.every = every;
-		this.alive = alive / this.every;
-		this.sleepCount = 0;
-		this.awakeCount = 0;
-		this.callbackCount = this.alive;
+		this._ip = ip;
+		this._every = every;
+		this._alive = alive / this._every;
+		this._count = [];
 
-		// Other state
+		// Published state
 		this.isSleep = undefined;
 		this.isAwake = undefined;
-		this.statusCallback = undefined;
-		this.mainLoop = undefined;
+
+		// Other state
+		this._statusCallback = undefined;
+		this._mainLoop = undefined;
 	}
 
 	connect = async (execute = true) => {
-		if(execute) {
+		if (execute) {
 			await this.subscribe();
 			this.emit("connected");
 		} else {
@@ -50,69 +49,71 @@ class ping extends EventEmitter {
 	ping = async () => {
 		return new Promise(async (resolve) => {
 			try {
-				execFile(`ping`, [`-c 1`, `-t 1`, this.ip], (error) => {
-					if(!error) resolve(false);
+				execFile(`ping`, [`-c 1`, `-t 1`, this._ip], (error) => {
+					if (error) resolve(false);
 					else resolve(true);
 				});
-			} catch(error) {
+			} catch (error) {
 				resolve(false);
 			}
 		});
 	}
 
 	subscribe = async () => {
-		return new Promise(async (resolve) => {
-			if(this.mainLoop) clearInterval(this.mainLoop);
-			this.mainLoop = setInterval(async () => {
-				let result = await this.ping();
-				if(result) {
-					this.sleepCount++;
-					this.awakeCount = 0;
-				} else {
-					this.sleepCount = 0;
-					this.awakeCount++;
-				}
+		function count(arr) {
+			let result = 0;
+			arr.forEach(el => {
+				if (el === 1) result++;
+			});
+			return result;
+		}
 
-				if(this.sleepCount > this.alive - 1) {
-					if (this.isSleep != true) {
-						this.isSleep = true;
-						this.isAwake = !this.isSleep;
-						this.emit("sleep");
-						resolve(this.isAwake);
-					}
-					this.sleepCount = 0;
-				} else if(this.awakeCount > this.alive - 1) {
-					if (this.isSleep != false) {
+		return new Promise(async (resolve) => {
+			if (this._mainLoop) clearInterval(this._mainLoop);
+			this._mainLoop = setInterval(async () => {
+				let result = await this.ping();
+
+				this._count.push(result ? 1 : 0);
+
+				if (this._count.length == this._alive + 1) {
+					this._count.shift();
+					result = count(this._count);
+
+					if (result == this._alive && !this.isAwake) {
 						this.isSleep = false;
 						this.isAwake = !this.isSleep;
-						this.emit("awake");
-						resolve(this.isAwake);
+						this.emit("awake", result, this.isAwake, this.isSleep);
+					} else if (result < this._alive && !this.isSleep) {
+						this.isSleep = true;
+						this.isAwake = !this.isSleep;
+						this.emit("sleep", result, this.isAwake, this.isSleep);
 					}
-					this.awakeCount = 0;
+
+					// Run status callback
+					if (this._statusCallback) {
+						if (this.isAwake == undefined) {
+							this.isSleep = true;
+							this.isAwake = !this.isSleep;
+						}
+
+						this._statusCallback(this.isAwake);
+						this._statusCallback = undefined;
+					}
+
+					resolve(this.isAwake);
 				}
 
-				// Run status callback
-				this.callbackCount--;
-				if(this.statusCallback && this.callbackCount <= 0) {
-					this.callbackCount = 0;
-
-					if(this.isSleep == undefined) this.isSleep = true;
-
-					this.statusCallback(!this.isSleep);
-					this.statusCallback = undefined;
-				}
-
-				this.emit(`update`);
-			}, this.every * 1000);
+				if (this.isAwake != undefined) this.emit(`update`, result, this.isAwake, this.isSleep);
+			}, this._every * 1000);
 		});
 	}
 
 	disconnect = () => {
-		if(this.mainLoop) clearInterval(this.mainLoop);
+		if (this._mainLoop) clearInterval(this._mainLoop);
 	}
 
-	status = (callback = () => {}) => {
-		this.statusCallback = callback;
+	status = (callback = () => { }) => {
+		this._statusCallback = callback;
 		return this.isAwake;
 	}
 }
--- test.js
+++ test.js
@@ -1,46 +1,36 @@
 var ping = require('../nodejs-ping-wrapper');
-var nSwitch = new ping('192.168.1.106', 20, 1);
+var nSwitch = new ping('192.168.1.106', 20, 5);
 
 function getDateTime() {
     var date = new Date();
 
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
+    var hour = (date.getHours() < 10 ? "0" : "") + date.getHours();
+    var min = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+    var sec =  (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
     var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
+    var month = ((date.getMonth() + 1) < 10 ? "0" : "") + (date.getMonth() + 1);
+    var day = (date.getDate() < 10 ? "0" : "") + date.getDate();
 
     return year + "-" + month + "-" + day + " " + hour + ":" + min + ":" + sec;
 }
 
-nSwitch.on('awake', function () {
-	console.log(getDateTime(), "TS: Nintendo Switch -> awake");
+nSwitch.on('awake', () => {
+    console.log(getDateTime(), "TS: Nintendo Switch -> awake");
 });
 
-nSwitch.on('sleep', function () {
-	console.log(getDateTime(), "TS: Nintendo Switch -> sleep");
+nSwitch.on('sleep', () => {
+    console.log(getDateTime(), "TS: Nintendo Switch -> sleep");
 });
 
-nSwitch.on(`update`, function () {
-    console.log(getDateTime(), `TS: Nintendo Switch -> Awake count: ${this.awakeCount * this.every}, sleep count: ${this.sleepCount * this.every}`);
+nSwitch.on(`update`, (count, awake, sleep) => {
+    console.log(getDateTime(), `TS: Nintendo Switch -> Count: ${count}, awake: ${awake}, sleep: ${sleep}`);
 });
 
-nSwitch.on(`connected`, function () {
-	console.log("TS: Nintendo Switch ready");
-	this.status((status) => {
-		console.log(getDateTime(), "TS: Nintendo Switch current status -> ", status ? "ON" : "OFF");
-	});
+nSwitch.on(`connected`, () => {
+    console.log("TS: Nintendo Switch ready");
+    nSwitch.status((status) => {
+        console.log(getDateTime(), "TS: Nintendo Switch current status -> ", status ? "ON" : "OFF");
+    });
 });
 
-nSwitch.connect().catch(e => console.log("Can't use net-ping"));
+nSwitch.connect().catch(e => console.log("Can't use net-ping", e));
