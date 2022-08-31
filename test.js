531e7d7ac4f695cd8b6d30d8da0cac69ac88e536
Dwan <me@dwaan.com>
2022-08-31 11:26:41 +0300

    updating test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var ping = require('../nodejs-ping-wrapper');
-var nSwitch = new ping('192.168.1.106', 22);
+var device = new ping('192.168.1.121', 40, 5);
 
 function getDateTime() {
     var date = new Date();
@@ -14,27 +14,27 @@ function getDateTime() {
     return year + "-" + month + "-" + day + " " + hour + ":" + min + ":" + sec;
 }
 
-nSwitch.on('awake', () => {
-    console.log(getDateTime(), "TS: Nintendo Switch -> awake");
+device.on('awake', () => {
+    console.log(getDateTime(), "TS: Device -> awake");
 });
 
-nSwitch.on('sleep', () => {
-    console.log(getDateTime(), "TS: Nintendo Switch -> sleep");
+device.on('sleep', () => {
+    console.log(getDateTime(), "TS: Device -> sleep");
 });
 
-nSwitch.on(`update`, (count, awake, sleep) => {
-    console.log(getDateTime(), `TS: Nintendo Switch -> Count: ${count}, awake: ${awake}, sleep: ${sleep}`);
+device.on(`update`, (count, awake, sleep) => {
+    console.log(getDateTime(), `TS: Device -> Count: ${count}, awake: ${awake}, sleep: ${sleep}`);
 });
 
-nSwitch.on(`connected`, () => {
-    console.log("TS: Nintendo Switch ready");
-    nSwitch.status((status) => {
-        console.log(getDateTime(), "TS: Nintendo Switch current status -> ", status ? "ON" : "OFF");
+device.on(`connected`, () => {
+    console.log(getDateTime(), "TS: Device ready");
+    device.status((status) => {
+        console.log(getDateTime(), "TS: Device current status -> ", status ? "ON" : "OFF");
     });
 });
 
-nSwitch.on(`before`, (count) => {
-    console.log("TS: count", count);
+device.on(`before`, (count) => {
+    console.log(getDateTime(), "TS: count", count);
 });
 
-nSwitch.connect().catch(e => console.log("Can't use net-ping", e));
+device.connect().catch(e => console.log(getDateTime(), "Can't use net-ping", e));
